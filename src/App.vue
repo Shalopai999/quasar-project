81dd81e639b745ee220e390ff8d212954f812b87
Andrey Nepomnyaschikh <And-dk@mail.ru>
2025-09-02 22:27:32 +0700

    careate project, begin meenu

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,85 @@
 <template>
-  <router-view />
+  <!-- <router-view /> -->
+  <div class="header">
+    <div class="header-mobile">
+      <div class="header-mobile__back">
+        <q-icon name="arrow_back" style="color: #ccc; font-size: 1.4em;"></q-icon>
+      </div>
+      <div class="header-mobile__search">
+        <!-- <q-icon name="search" style="color: #ccc; font-size: 1.4em;"></q-icon> -->
+        <!-- <input class="header-mobile__input" type="text" placeholder="Поиск по сайту"> -->
+        <q-input v-model="search" debounce="500" placeholder="Search">
+          <template v-slot:append>
+            <q-icon name="search"></q-icon>
+          </template>
+        </q-input>
+      </div>
+      <div class="header-mobile__notification">
+        <q-icon color="#afafaf" icon="mdi-bell-outline" size="large"></q-icon>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
 //
 </script>
+
+<style scoped lang="scss">
+.header {
+  min-height: 60px;
+  box-shadow: 0 1px 4px #00000029;
+}
+
+.header-mobile {
+
+  min-height: 60px;
+  padding: 12px 16px;
+  gap: 8px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  &__back {
+    width: 36px;
+    height: 36px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 8px;
+    background-color: #f7f7f7;
+    flex-shrink: 0;
+  }
+
+  &__search {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    height: 36px;
+    padding: 0px 8px 0px 8px;
+    border-radius: 8px;
+    background-color: #f7f7f7;
+  }
+
+  &__input {
+    width: 100%;
+    padding: 0px 8px 0px 8px;
+  }
+
+  &__notification {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 8px;
+    background-color: #f7f7f7;
+    width: 36px;
+    height: 36px;
+    flex-shrink: 0;
+  }
+}
+
+.v-main {
+  background-color: rgb(246, 246, 246);
+}
+</style>
